6b4946efd0760c6adb9dc249e88f1b60c3ae541a
Mateus Ribeiro <mateusrfon@gmail.com>
2021-07-29 10:51:08 -0300

    test without database

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,17 @@ import express from "express";
 import cors from "cors";
 import "reflect-metadata";
 
-import connectDatabase from "./database";
+//import connectDatabase from "./database";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/test", (req, res) => {
+    res.sendStatus(201);
+})
+
 export default app;
-export async function init() {
+/*export async function init() {
     await connectDatabase();
-}
+}*/
