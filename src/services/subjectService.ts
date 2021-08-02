f33094d702c15e0f7832db0c0a080506c81a75ad
Mateus Ribeiro <mateusrfon@gmail.com>
2021-08-02 12:44:25 -0300

    add exam category on subject by id

--- src/services/subjectService.ts
+++ src/services/subjectService.ts
@@ -11,7 +11,7 @@ export async function findAll() {
 
 export async function findById(id: number) {
     try {
-        return await getRepository(Subject).findOne({ relations: ['exams'], where: { id } });
+        return await getRepository(Subject).findOne({ relations: ['exams', 'exams.category'], where: { id } });
     } catch(error) {
         console.error(error);
     }   
